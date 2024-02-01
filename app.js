ae3d1f51a873824193d0bb6ff4748ce3667d0e9f
cam06002 <jeffcampbell333@gmail.com>
2024-02-01 15:26:51 -0800

    connected body-parser

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const fileRoutes = require('./routes/file-routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/files', fileRoutes);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unknown error occurred!'});
+});
 
 app.listen(5000);
