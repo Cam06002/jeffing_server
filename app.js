39fac02368a96b622801318fd3ba16a842bf07d1
cam06002 <jeffcampbell333@gmail.com>
2024-02-07 12:29:34 -0800

    added cors headers

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
+
 app.use('/api/files', fileRoutes);
 app.use('/api/auth', authRoutes);
 
