b6714b750d9a13ae8efd8ef27bc41ea7765e9f50
cam06002 <jeffcampbell333@gmail.com>
2024-02-08 16:00:54 -0800

    added user object to login response

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -18,7 +18,7 @@ const login = async (req, res, next) => {
         return next(new HttpError('Invalid Credentials.', 401));
     }
    
-    res.json({message: 'Logged in!'});
+    res.json({message: 'Logged in!', user: existingUser.toObject({getters: true})});
 }
 
 const register = async (req, res, next) => {
