22d5779f0d80b0048839d2ee9137d55462b0a579
cam06002 <jeffcampbell333@gmail.com>
2024-02-16 08:57:42 -0800

    added yaml file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
     res.setHeader('Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
@@ -37,7 +37,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.p6uc4vv.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(process.env.PORT || 5000);
+        app.listen(process.env.PORT || 8080);
     })
     .catch( err => {
         console.log(err);
