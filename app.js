beccd6ca4d5917dd55d905dd421bb54481a87bcc
cam06002 <jeffcampbell333@gmail.com>
2024-02-16 12:24:05 -0800

    updated connections

--- app.js
+++ app.js
@@ -35,9 +35,8 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@${process.env.DB_NAME}.p6uc4vv.mongodb.net/?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.p6uc4vv.mongodb.net/?retryWrites=true&w=majority`)
     .then(() => {
-        app.listen(process.env.PORT || 8080);
     })
     .catch( err => {
         console.log(err);
