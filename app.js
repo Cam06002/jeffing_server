1b8f5a3476f623121dfed0038216771dc2e7e0f9
cam06002 <jeffcampbell333@gmail.com>
2024-02-16 14:15:42 -0800

    updated access

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://jeffingtext.web.app');
     res.setHeader('Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
