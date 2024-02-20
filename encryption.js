b4bd2d7772aecd465a05041b5ac36a8785d2c97c
cam06002 <jeffcampbell333@gmail.com>
2024-02-20 11:29:51 -0800

    updated encryption

--- encryption.js
+++ encryption.js
@@ -1,14 +1,14 @@
 const crypto = require('crypto');
 
 const encryptEditor = (editor, secretKey)=>{
-    const eCipher = crypto.createCipher('aes-256-gcm', secretKey);
+    const eCipher = crypto.createCipher('aes-256-cbc', secretKey);
     let encryptedEditor = eCipher.update(editor, 'utf-8', 'hex');
     encryptedEditor += eCipher.final('hex');
     return encryptedEditor
 }
 
 const decryptEditor = (encryptedEditor, secretKey) => {
-    const dCipher = crypto.createDecipher('aes-256-gcm', secretKey);
+    const dCipher = crypto.createDecipher('aes-256-cbc', secretKey);
     let decryptedEditor = dCipher.update(encryptedEditor, 'hex', 'utf-8');
     decryptedEditor += dCipher.final('utf-8');
     return decryptedEditor;
