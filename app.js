d35a738f7d757bc509000ebe96218aaa5671137b
Jeff Campbell <jeffcampbell333@gmail.com>
2024-03-04 19:03:09 -0800

    added originlist and logic to include localhost as allowed origin

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const fileRoutes = require('./routes/file-routes');
 const authRoutes = require('./routes/auth-routes');
 
 const app = express();
+const originList = ['https://jeffingtext.web.app', 'http://localhost:3000']
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://jeffingtext.web.app');
+    let origin = originList.includes(req.header.origin) ? req.header.origin : originList[0];
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Headers', 
     'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
